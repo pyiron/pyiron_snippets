a1c2ccee3138bc1bd7779e30abe6c6d3e947e084
liamhuber <liamhuber@greyhavensolutions.com>
2024-05-27 15:12:56 -0700

    Move setup into tests
    None of it was shared

--- tests/unit/test_import_alarm.py
+++ tests/unit/test_import_alarm.py
@@ -4,23 +4,41 @@ from snippets.import_alarm import ImportAlarm, ImportAlarmError
 
 
 class TestImportAlarm(unittest.TestCase):
-    def setUp(self):
-        super().setUp()
-        self.no_alarm = ImportAlarm(_fail_on_warning=True)
 
-        @self.no_alarm
+    def test_instance(self):
+        no_alarm = ImportAlarm(_fail_on_warning=True)
+
+        @no_alarm
         def add_one(x):
             return x + 1
 
-        self.yes_alarm = ImportAlarm(
+        yes_alarm = ImportAlarm(
             "Here is a message",
             _fail_on_warning=True
         )
 
-        @self.yes_alarm
+        @yes_alarm
         def subtract_one(x):
             return x + 1
 
+        self.assertEqual(
+            1,
+            add_one(0),
+            msg="Without a message, the import alarm should not raise a warning (an "
+                "exception in this case, because of the private flag)"
+        )
+        with self.assertRaises(
+            ImportAlarmError,
+            msg="With a message, the import alarm should raise a warning. (an "
+                "exception in this case, because of the private flag)"
+        ):
+            subtract_one(0)
+
+    def test_context(self):
+        """
+        Usage via context manager should give same results and not suppress other
+        errors.
+        """
         with ImportAlarm(
             "Working import",
             _fail_on_warning=True
@@ -42,33 +60,9 @@ class TestImportAlarm(unittest.TestCase):
         def add_three(x):
             return x + 3
 
-        self.add_one = add_one
-        self.subtract_one = subtract_one
-        self.add_two = add_two
-        self.add_three = add_three
-
-    def test_instance(self):
-        self.assertEqual(
-            1,
-            self.add_one(0),
-            msg="Without a message, the import alarm should not raise a warning (an "
-                "exception in this case, because of the private flag)"
-        )
-        with self.assertRaises(
-            ImportAlarmError,
-            msg="With a message, the import alarm should raise a warning. (an "
-                "exception in this case, because of the private flag)"
-        ):
-            self.subtract_one(0)
-
-    def test_context(self):
-        """
-        Usage via context manager should give same results and not suppress other
-        errors.
-        """
         self.assertEqual(
             2,
-            self.add_two(0),
+            add_two(0),
             msg="Without a message, no warning (exception here) should be raised"
         )
 
@@ -76,7 +70,7 @@ class TestImportAlarm(unittest.TestCase):
             ImportAlarmError,
             msg="With a message, a warning (exception here) should be raised"
         ):
-            self.add_three(0)
+            add_three(0)
 
     def test_scope(self):
         with self.assertRaises(
