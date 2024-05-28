f7ebb91d138f0d343920c864fbe7966a9b6c44bf
Liam Huber <liamhuber@greyhavensolutions.com>
2024-05-28 09:12:36 -0700

    Base stuff (#6)
    * move tests_base to tests
    
    * Replace pyiron.base with pyiron_base
    
    * Add decorator for deprecation warnings
    
    * Add support for pending deprecation
    
    * Fix codacy nags
    
    * Fix docstring markup
    
    * Add ability to deprecate single arguments
    
    * Allow passing deprecated arguments via kwargs
    
    * Add a util class for import warnings (#116)
    
    * Add a util class for import warnings
    
    * Add docstring
    
    * Add test
    
    * Add context manager
    
    Co-authored-by: Marvin Poul <ponder@creshal.de>
    
    * Copy deprecator instance on decorating
    
    Decorating a function with the Deprecator can change its instance
    attributes (if you pass arguments when decorating).  That means that
    when decorating different functions the warning shown used only the
    arguments from the last call to the deprecator instance.
    
    This is fixed now by copying the deprecator instance before wrapping the
    given function.
    
    * Using the new TestCase module
    
    * Fixing imports
    
    * Refactoring
    
    * Corrections as discussed in the pyiron meeting
    
    * black formatting
    
    * Add function to retry until error goes away
    
    We had a pattern for this in multiple places (talking to the database or
    hdf5 file).
    
    * Add tests
    
    * Small nits
    
    * Fix typo
    
    * Format black
    
    * Small nits
    
    * Rename atmost -> at_most
    
    * Rename atmost -> at_most
    
    * Call all the single supers!
    
    * add ruff linter
    
    * add ruff linter
    
    * Move files into snippets hierarchy
    
    * Sanitize deprecator
    
    * Remove tests for stuff that wasn't transferred
    
    * Split retry and import_alarm
    
    * Move the missed test file into the snippets hierarchy
    
    * Refactor: rename test file
    
    * Break apart deprecate and import_alarm tests
    
    * Fix deprecator tests
    
    * Fix retry tests
    
    * Update import_alarm tests
    
    * Fix tests to catch failures instead of warnings
    
    Catching warnings can be janky, e.g. if some upstream dependency starts getting a deprecation warning (possible even depending only on the standard library, as we may update python itself), so provide a private way to convert those warnings into a hard failure for test purposes.
    
    * Move setup into tests
    
    None of it was shared
    
    * Remove unused import
    
    * Remove outdated docstring
    
    * Update tests/unit/test_import_alarm.py
    
    Co-authored-by: Marvin Poul <ponder@creshal.de>
    
    * Update tests/unit/test_import_alarm.py
    
    Co-authored-by: Marvin Poul <ponder@creshal.de>
    
    * Update snippets/deprecate.py
    
    Co-authored-by: Sam Dareska <37879103+samwaseda@users.noreply.github.com>
    
    * Update snippets/deprecate.py
    
    Co-authored-by: Sam Dareska <37879103+samwaseda@users.noreply.github.com>
    
    * Update snippets/deprecate.py
    
    Co-authored-by: Sam Dareska <37879103+samwaseda@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jan Janßen <janssen@mpie.de>
    Co-authored-by: Marvin Poul <ponder@creshal.de>
    Co-authored-by: Marvin Poul <poul@mpie.de>
    Co-authored-by: sudarsan1989 <surendralal@mpie.de>
    Co-authored-by: Jan Janssen <jan.janssen@outlook.com>
    Co-authored-by: Jan Janssen <jan-janssen@users.noreply.github.com>
    Co-authored-by: pyiron-runner <pyiron@mpie.de>
    Co-authored-by: Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
    Co-authored-by: Sam Dareska <37879103+samwaseda@users.noreply.github.com>

---
+++ tests/unit/test_import_alarm.py
@@ -0,0 +1,82 @@
+import unittest
+from snippets.import_alarm import ImportAlarm, ImportAlarmError
+
+
+class TestImportAlarm(unittest.TestCase):
+
+    def test_instance(self):
+        no_alarm = ImportAlarm(_fail_on_warning=True)
+
+        @no_alarm
+        def add_one(x):
+            return x + 1
+
+        yes_alarm = ImportAlarm(
+            "Here is a message",
+            _fail_on_warning=True
+        )
+
+        @yes_alarm
+        def subtract_one(x):
+            return x + 1
+
+        try:
+            self.assertEqual(
+                1,
+                add_one(0),
+                msg="Wrapped function should return the same return value."
+            )
+        except ImportAlarmError:
+            self.fail("Without a message, the import alarm should not raise a warning (an "
+                    "exception in this case, because of the private flag)")
+        with self.assertRaises(
+            ImportAlarmError,
+            msg="With a message, the import alarm should raise a warning. (an "
+                "exception in this case, because of the private flag)"
+        ):
+            subtract_one(0)
+
+    def test_context(self):
+        with ImportAlarm(
+            "Working import",
+            _fail_on_warning=True
+        ) as alarm_working:
+            # Suppose all the imports here pass fine
+            pass
+
+        with ImportAlarm(
+            "Broken import",
+            _fail_on_warning=True
+        ) as alarm_broken:
+            raise ImportError("Suppose a package imported here is not available")
+
+        @alarm_working
+        def add_two(x):
+            return x + 2
+
+        @alarm_broken
+        def add_three(x):
+            return x + 3
+
+        self.assertEqual(
+            2,
+            add_two(0),
+            msg="Without a message, no warning (exception here) should be raised"
+        )
+
+        with self.assertRaises(
+            ImportAlarmError,
+            msg="With a message, a warning (exception here) should be raised"
+        ):
+            add_three(0)
+
+    def test_scope(self):
+        with self.assertRaises(
+            ZeroDivisionError,
+            msg="Context manager should not silence unrelated exceptions"
+        ), ImportAlarm("Unrelated"):
+            print(1 / 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
