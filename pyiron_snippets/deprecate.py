ed11f84c0a020318358d3d06a0f6572ee36dc0f8
liamhuber <liamhuber@greyhavensolutions.com>
2025-10-06 10:05:11 -0700

    Add missing f-command
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- pyiron_snippets/deprecate.py
+++ pyiron_snippets/deprecate.py
@@ -146,7 +146,7 @@ class Deprecator:
                 if kw in self.arguments:
                     warnings.warn(
                         message_format.format(
-                            "{function.__module__}.{function.__name__}({kw}={kwargs[kw]})"
+                            f"{function.__module__}.{function.__name__}({kw}={kwargs[kw]})"
                         ),
                         category=self.category,
                         stacklevel=2,
