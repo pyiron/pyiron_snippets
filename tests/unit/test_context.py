b77b9a2b5c8a68332380f35c5bf87dcbf0854ce7
Jan Janssen <janssen@mpie.de>
2025-10-13 17:06:27 +0200

    Add directory context

---
+++ tests/unit/test_context.py
@@ -0,0 +1,13 @@
+import os
+import unittest
+
+from pyiron_snippets.context import set_directory
+
+
+class TestContextDirectory(unittest.TestCase):
+    def test_set_directory(self):
+        current_directory = os.getcwd()
+        test_directory = "context"
+        with set_directory(path=test_directory):
+            context_directory = os.getcwd()
+        self.assertEqual(os.path.relpath(context_directory, current_directory), test_directory)
